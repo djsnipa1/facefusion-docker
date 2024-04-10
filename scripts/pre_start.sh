b49f80830b45a534372fd03df3ab509bbbc83d50
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2024-04-10 14:58:05 +0200

    Remove syncing of venv since there is no longer a venv

--- scripts/pre_start.sh
+++ scripts/pre_start.sh
@@ -6,7 +6,6 @@ export APP="facefusion"
 DOCKER_IMAGE_VERSION_FILE="/workspace/${APP}/docker_image_version"
 
 echo "Template version: ${TEMPLATE_VERSION}"
-echo "venv: ${VENV_PATH}"
 
 if [[ -e ${DOCKER_IMAGE_VERSION_FILE} ]]; then
     EXISTING_VERSION=$(cat ${DOCKER_IMAGE_VERSION_FILE})
@@ -21,29 +20,16 @@ else
 fi
 
 sync_apps() {
-    # Sync venv to workspace to support Network volumes
-    echo "Syncing venv to workspace, please wait..."
-    mkdir -p ${VENV_PATH}
-    rsync --remove-source-files -rlptDu /venv/ ${VENV_PATH}/
-
     # Sync application to workspace to support Network volumes
     echo "Syncing ${APP} to workspace, please wait..."
     rsync --remove-source-files -rlptDu /${APP}/ /workspace/${APP}/
 
     echo "${TEMPLATE_VERSION}" > ${DOCKER_IMAGE_VERSION_FILE}
-    echo "${VENV_PATH}" > "/workspace/${APP}/venv_path"
-}
-
-fix_venvs() {
-    # Fix the venv to make it work from VENV_PATH
-    echo "Fixing venv..."
-    /fix_venv.sh /venv ${VENV_PATH}
 }
 
 if [ "$(printf '%s\n' "$EXISTING_VERSION" "$TEMPLATE_VERSION" | sort -V | head -n 1)" = "$EXISTING_VERSION" ]; then
     if [ "$EXISTING_VERSION" != "$TEMPLATE_VERSION" ]; then
         sync_apps
-        fix_venvs
 
         # Create directories
         mkdir -p /workspace/logs /workspace/tmp
